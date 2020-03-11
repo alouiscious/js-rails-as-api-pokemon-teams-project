ae2f95ddcfb2ae52f2354a41df88faf9e63925b2
Alouiscious Media <info@reneesoul.com>
2020-03-10 21:45:31 -0700

    added fast json and serializers

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,22 @@
 class TrainersController < ApplicationController
+  def index
+    trainers = Trainer.all
+    render json: TrainerSerializer.new(trainers)
+  end
+
+    # render json: TrainerSerializer.new(@trainers).to_serialized_json
+    # render json: trainers.to_json(:include => {
+    #   :pokemon => {
+    #     :only => [:name, :species, :nickname]
+    #   }
+    # })
+
+  def show
+    trainer = Trainer.find_by(id: params[:id])
+    # options = { include: [:pokemon] }
+    render json: TrainerSerializer.new(trainer)
+    # render json: TrainerSerializer.new(trainer, options).to_serialized_json
+  end
+
+
 end
